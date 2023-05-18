7bb44590e1537a19222703c591392a231092bde1
rootsuke <rootsuke@gmail.com>
2023-05-18 23:08:16 +0900

    update comments

--- search/hash_search.go
+++ search/hash_search.go
@@ -1,12 +1,14 @@
 package search
 
-// 0 ~ n-1
+// 1次ハッシュ
 func primaryHash(key int, n int) int {
 	return key % n
 }
 
-// 1 ~ n-1
+// 2次ハッシュ
 func secondaryHash(key int, n int) int {
+	// 再計算したハッシュ値が同じにならないように
+	// 1以上かつハッシュテーブルの長さ未満の値を返す
 	return 1 + (key % (n - 1))
 }
 
@@ -17,14 +19,18 @@ func calcHash(key int, i int, n int) int {
 func Insert(hashTable []int, key int) int {
 	n := len(hashTable)
 	i := 0
+
 	for i < n {
 		hash := calcHash(key, i, n)
 		if hashTable[hash] == 0 {
+			// 格納しそのハッシュ値を返す
 			hashTable[hash] = key
 			return hash
 		} else if hashTable[hash] == key {
+			// すでに同じ値が格納されていればそのハッシュ値を返す
 			return hash
 		} else {
+			// 衝突した場合はハッシュ値を再計算する
 			i++
 		}
 	}
@@ -38,10 +44,13 @@ func Search(hashTable []int, key int) int {
 	for i < n {
 		hash := calcHash(key, i, n)
 		if hashTable[hash] == key {
+			// 見つかったらハッシュ値を返す
 			return hash
 		} else if hashTable[hash] == 0 {
+			// 空だった場合は格納されていないと判定し-1を返す
 			return -1
 		} else {
+			// 違う値が格納されていた場合はハッシュ値を再計算する
 			i++
 		}
 	}
