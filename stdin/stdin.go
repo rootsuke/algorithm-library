fa911d91446bdccce46f4812d7b5b0e66fd060d4
rootsuke <rootsuke@gmail.com>
2023-05-20 13:45:42 +0900

    update stdin library

--- stdin/stdin.go
+++ stdin/stdin.go
@@ -6,22 +6,33 @@ import (
 	"strconv"
 )
 
-var sc = bufio.NewScanner(os.Stdin)
-
-func NextString() string {
-	sc.Scan()
-	return sc.Text()
+type Scanner struct {
+	sc *bufio.Scanner
 }
 
-func NextInt() int {
-	n, _ := strconv.Atoi(NextString())
-	return n
+func NewScanner() *Scanner {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 1024), int(1e+9))
+	return &Scanner{sc}
 }
 
-func NextIntSlice(size int) []int {
+func (s *Scanner) nextStr() string {
+	s.sc.Scan()
+	return s.sc.Text()
+}
+
+func (s *Scanner) nextInt() int {
+	i, e := strconv.Atoi(s.nextStr())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+func (s *Scanner) nextIntSlice(size int) []int {
 	res := make([]int, size)
 	for i := 0; i < size; i++ {
-		res[i] = NextInt()
+		res[i] = s.nextInt()
 	}
 	return res
 }
