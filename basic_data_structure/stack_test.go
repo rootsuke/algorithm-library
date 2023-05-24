66f644ed621149d55bd6e2dd6aa53591d580b6f3
rootsuke <rootsuke@gmail.com>
2023-05-25 00:28:02 +0900

    add testcase

--- basic_data_structure/stack_test.go
+++ basic_data_structure/stack_test.go
@@ -6,15 +6,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSearchTargetByBinarySearch(t *testing.T) {
+func TestStack(t *testing.T) {
 	stack := &Stack{}
 	stack.Push(1)
 	stack.Push(2)
 
-	b := stack.Pop()
+	assert.Equal(t, 1, stack.stack[0])
+	assert.Equal(t, 2, stack.stack[1])
+
 	a := stack.Pop()
+	b := stack.Pop()
 
 	// 後入れ先出しになっていることを確認
-	assert.Equal(t, 1, a)
-	assert.Equal(t, 2, b)
+	assert.Equal(t, 2, a)
+	assert.Equal(t, 1, b)
 }
